d3d359f3feb30d44c92be92ffb2d932937517be7
Igor Bylinkin <kaspr88@mail.ru>
2022-12-14 21:45:31 +0600

    Задача с собакой

--- 007/Program.cs
+++ 007/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SpeedFriendOne = 1;
+int SpeedFriendTwo = 2;
+int SpeedDog = 5;
+int Distance = 10000;
+int Friend = 1;
+int caunt = 0;
+int time;
+while (Distance > 10)
+{
+    if (Friend == 1)
+    {
+        time = Distance / (SpeedFriendOne + SpeedDog);
+        Friend = 2;
+        Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
+        caunt = caunt + 1;
+    }
+    if (Friend == 2)
+    {
+        time = Distance / (SpeedFriendOne + SpeedDog);
+        Friend = 1;
+        Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
+        caunt = caunt + 1;
+    }
+
+}
+Console.Write(caunt);
