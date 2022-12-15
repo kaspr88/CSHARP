f67a1d85bc82def89b5e84d2c45d3c15b59dc3b9
Igor Bylinkin <kaspr88@mail.ru>
2022-12-16 01:10:23 +0600

    Все задачи

---
+++ 011/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string Read = Console.ReadLine() ?? "";
+int a = int.Parse(Read);
+if (a == 1) Console.WriteLine("Понедельник");
+else if (a == 2) Console.WriteLine("Вторник");
+else if (a == 3) Console.WriteLine("Среда");
+else if (a == 4) Console.WriteLine("Четверг");
+else if (a == 5) Console.WriteLine("Пятница");
+else if (a == 6) Console.WriteLine("Суббота");
+else if (a == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("не день недели");
