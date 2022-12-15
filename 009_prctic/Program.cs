28ca5ca1b0ae64b41707d8448eb87cfd42f207d2
Igor Bylinkin <kaspr88@mail.ru>
2022-12-16 01:12:50 +0600

    Все задачи1

--- 009_prctic/Program.cs
+++ 009_prctic/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+string userInput = Console.ReadLine() ?? ""; //Конструкция (?? "") убирает желтую ошибку из консоли
+int number = int.Parse(userInput);//Метод Parse превращвет меняет тип
+int result = (int)Math.Pow(number, 2); //Явное приведение типов (int) Метод (Pow.класс) математические функции
+Console.WriteLine($"{number}->{result}");// способ вывести 2 переменные
---
+++ 010/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = Console.ReadLine();
+int b = Console.ReadLine();
+int a = int.Parse(userInput);
+int b = int.Parse(userInput);
+if (a = b * b)
+{
+    Console.WriteLine($"{a}квадрат{b}");
+}
+else if (b = b * b)
+{
+    Console.WriteLine($"{a} не квадрат{b}");
+}
+else
+{
+    Console.WriteLine($"квадрата нет");
+}
